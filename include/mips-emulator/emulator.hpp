0ce74c10bcbe9e6e6ede29a693f069d7bebf54e3
antlilja <liljaanton2001@gmail.com>
2022-01-20 13:09:49 +0100

    Renamed get_register_file to clone_register_file

--- include/mips-emulator/emulator.hpp
+++ include/mips-emulator/emulator.hpp
@@ -14,7 +14,7 @@ namespace mips_emulator {
         template <typename... Args>
         Emulator(Args&&... args) : memory(std::forward<Args>(args)...) {}
 
-        RegisterFile get_register_file() const noexcept { return reg_file; }
+        RegisterFile clone_register_file() const noexcept { return reg_file; }
 
         void step() noexcept { Executor::step(reg_file, memory, pc); }
 

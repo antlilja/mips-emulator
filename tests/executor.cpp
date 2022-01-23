bd16368b4e40e204c226e8f01d39cd70dc5ad3a4
Brynolv <k.jzfgxgh@gmail.com>
2022-01-23 13:54:47 +0100

    Added OR-test
    To try how this works. Based entirely
    on add test, but it seems to work.

--- tests/executor.cpp
+++ tests/executor.cpp
@@ -27,3 +27,22 @@ TEMPLATE_TEST_CASE("add", "[Executor]", RegisterFile32, RegisterFile64) {
         REQUIRE(reg_file.get(RegisterName::e_t2).s == 6);
     }
 }
+
+TEMPLATE_TEST_CASE("or", "[Executor]", RegisterFile32, RegisterFile64) {
+	SECTION("Positive numbers") {
+		using Address = typename TestType::Unsigned;
+		TestType reg_file;
+		Address pc = 0;
+
+		reg_file.set_unsigned(RegisterName::e_t0, 0b1); // 1
+		reg_file.set_unsigned(RegisterName::e_t1, 0b110); // 6
+
+		Instruction instr(Func::e_or, RegisterName::e_t2, RegisterName::e_t0,
+				RegisterName::e_t1);
+
+		const bool no_error = Executor::handle_rtype_instr(instr, pc, reg_file);
+		REQUIRE(no_error);
+
+		REQUIRE(reg_file.get(RegisterName::e_t2).u == 0b111); // 7
+	}
+}

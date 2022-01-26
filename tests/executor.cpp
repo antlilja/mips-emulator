f43e06e45972a2004e3d76b4516c74f11f460434
antlilja <liljaanton2001@gmail.com>
2022-01-26 15:45:24 +0100

    Moved PC into register file.

--- tests/executor.cpp
+++ tests/executor.cpp
@@ -14,7 +14,6 @@ TEMPLATE_TEST_CASE("add", "[Executor]", RegisterFile32, RegisterFile64) {
     SECTION("Positive numbers") {
         using Address = typename TestType::Unsigned;
         TestType reg_file;
-        Address pc = 0;
 
         reg_file.set_signed(RegisterName::e_t0, 1);
         reg_file.set_signed(RegisterName::e_t1, 5);
@@ -22,7 +21,7 @@ TEMPLATE_TEST_CASE("add", "[Executor]", RegisterFile32, RegisterFile64) {
         Instruction instr(Func::e_add, RegisterName::e_t2, RegisterName::e_t0,
                           RegisterName::e_t1);
 
-        const bool no_error = Executor::handle_rtype_instr(instr, pc, reg_file);
+        const bool no_error = Executor::handle_rtype_instr(instr, reg_file);
         REQUIRE(no_error);
 
         REQUIRE(reg_file.get(RegisterName::e_t2).s == 6);
@@ -33,7 +32,6 @@ TEMPLATE_TEST_CASE("sub", "[Executor]", RegisterFile32, RegisterFile64) {
     SECTION("Positive numbers") {
         using Address = typename TestType::Unsigned;
         TestType reg_file;
-        Address pc = 0;
 
         reg_file.set_signed(RegisterName::e_t0, 10);
         reg_file.set_signed(RegisterName::e_t1, 1);
@@ -41,13 +39,13 @@ TEMPLATE_TEST_CASE("sub", "[Executor]", RegisterFile32, RegisterFile64) {
         Instruction instr(Func::e_sub, RegisterName::e_t2, RegisterName::e_t0,
                           RegisterName::e_t1);
 
-        const bool no_error = Executor::handle_rtype_instr(instr, pc, reg_file);
+        const bool no_error = Executor::handle_rtype_instr(instr, reg_file);
         REQUIRE(no_error);
 
         REQUIRE(reg_file.get(RegisterName::e_t2).s == 9);
     }
 
-	SECTION("Negative numbers") {
+    SECTION("Negative numbers") {
         using Address = typename TestType::Unsigned;
         TestType reg_file;
         Address pc = 0;
@@ -58,7 +56,7 @@ TEMPLATE_TEST_CASE("sub", "[Executor]", RegisterFile32, RegisterFile64) {
         Instruction instr(Func::e_sub, RegisterName::e_t2, RegisterName::e_t0,
                           RegisterName::e_t1);
 
-        const bool no_error = Executor::handle_rtype_instr(instr, pc, reg_file);
+        const bool no_error = Executor::handle_rtype_instr(instr, reg_file);
         REQUIRE(no_error);
 
         REQUIRE(reg_file.get(RegisterName::e_t2).s == 2);
@@ -66,38 +64,36 @@ TEMPLATE_TEST_CASE("sub", "[Executor]", RegisterFile32, RegisterFile64) {
 }
 
 TEMPLATE_TEST_CASE("or", "[Executor]", RegisterFile32, RegisterFile64) {
-	SECTION("Positive numbers") {
-		using Address = typename TestType::Unsigned;
-		TestType reg_file;
-		Address pc = 0;
+    SECTION("Positive numbers") {
+        using Address = typename TestType::Unsigned;
+        TestType reg_file;
 
-		reg_file.set_unsigned(RegisterName::e_t0, 0b1); // 1
-		reg_file.set_unsigned(RegisterName::e_t1, 0b110); // 6
+        reg_file.set_unsigned(RegisterName::e_t0, 0b1);   // 1
+        reg_file.set_unsigned(RegisterName::e_t1, 0b110); // 6
 
-		Instruction instr(Func::e_or, RegisterName::e_t2, RegisterName::e_t0,
-				RegisterName::e_t1);
+        Instruction instr(Func::e_or, RegisterName::e_t2, RegisterName::e_t0,
+                          RegisterName::e_t1);
 
-		const bool no_error = Executor::handle_rtype_instr(instr, pc, reg_file);
-		REQUIRE(no_error);
+        const bool no_error = Executor::handle_rtype_instr(instr, reg_file);
+        REQUIRE(no_error);
 
-		REQUIRE(reg_file.get(RegisterName::e_t2).u == 0b111); // 7
-	}
+        REQUIRE(reg_file.get(RegisterName::e_t2).u == 0b111); // 7
+    }
 }
 
 TEMPLATE_TEST_CASE("addi", "[Executor]", RegisterFile32, RegisterFile64) {
-	SECTION("Positive numbers") {
-		using Address = typename TestType::Unsigned;
-		TestType reg_file;
-		Address pc = 0;
+    SECTION("Positive numbers") {
+        using Address = typename TestType::Unsigned;
+        TestType reg_file;
 
-		reg_file.set_unsigned(RegisterName::e_t0, 100202);
+        reg_file.set_unsigned(RegisterName::e_t0, 100202);
 
-		Instruction instr(IOp::e_addi, RegisterName::e_t0, RegisterName::e_t1,
-			22020);
+        Instruction instr(IOp::e_addi, RegisterName::e_t0, RegisterName::e_t1,
+                          22020);
 
-		const bool no_error = Executor::handle_itype_instr(instr, pc, reg_file);
-		REQUIRE(no_error);
+        const bool no_error = Executor::handle_itype_instr(instr, reg_file);
+        REQUIRE(no_error);
 
-		REQUIRE(reg_file.get(RegisterName::e_t1).u == 122222);
-	}
+        REQUIRE(reg_file.get(RegisterName::e_t1).u == 122222);
+    }
 }

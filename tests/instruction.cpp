bae0c0e3725df2596679faea95ba7d283e9aa801
antlilja <liljaanton2001@gmail.com>
2022-01-27 14:08:41 +0100

    Added tests for Instruction union.
    Added tests for R and I type instructions (add and addi).

--- tests/instruction.cpp
+++ tests/instruction.cpp
@@ -1,7 +1,42 @@
 #include "mips-emulator/instruction.hpp"
+#include "mips-emulator/register_name.hpp"
 
 #include <catch2/catch.hpp>
 
 using namespace mips_emulator;
 
-// TODO: Write tests for instruction union
+using Func = Instruction::Func;
+using IOp = Instruction::ITypeOpcode;
+using JOp = Instruction::JTypeOpcode;
+
+TEST_CASE("R-Type instruction", "[Instruction]") {
+    SECTION("add - zero registers") {
+        const Instruction instr(Func::e_add, RegisterName::e_0,
+                                RegisterName::e_0, RegisterName::e_0);
+
+        REQUIRE(instr.raw == 0x20);
+    }
+
+    SECTION("add - non zero registers") {
+        const Instruction instr(Func::e_add, RegisterName::e_t0,
+                                RegisterName::e_t5, RegisterName::e_a0);
+
+        REQUIRE(instr.raw == 0x01a44020);
+    }
+}
+
+TEST_CASE("I-Type instruction", "[Instruction]") {
+    SECTION("addi - zero registers and zero imm") {
+        const Instruction instr(IOp::e_addi, RegisterName::e_0,
+                                RegisterName::e_0, 0);
+
+        REQUIRE(instr.raw == 0x20000000);
+    }
+
+    SECTION("addi - non zero registers and imm") {
+        const Instruction instr(IOp::e_addi, RegisterName::e_t0,
+                                RegisterName::e_t5, 0xffff);
+
+        REQUIRE(instr.raw == 0x21a8ffff);
+    }
+}

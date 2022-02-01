e3937ca28674b2892fa50b09317a5f692f563202
antlilja <liljaanton2001@gmail.com>
2022-02-01 13:55:33 +0100

    Added Span type
    The Span type is a view into a memory region, read and write.

---
+++ include/mips-emulator/span.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+namespace mips_emulator {
+    template <typename T>
+    class Span {
+    public:
+        Span(T* data, std::size_t size) : data(data), size(size) {}
+
+        T* get_data() noexcept { data; }
+        const T* get_data() const noexcept { data; }
+
+        std::size_t get_size() const noexcept { return size; }
+
+        T& operator[](std::size_t i) { return data[i]; }
+        const T& operator[](std::size_t i) const { return data[i]; }
+
+    private:
+        T* data;
+        std::size_t size;
+    };
+} // namespace mips_emulator

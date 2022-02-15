1c3829fa26089bfec0cda8974500270f23894f11
Emil G <eim.wco@gmail.com>
2022-02-15 15:07:31 +0100

    remove RegisterFile template from Emulator class.

--- include/mips-emulator/emulator.hpp
+++ include/mips-emulator/emulator.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include "mips-emulator/instruction.hpp"
 #include "mips-emulator/executor.hpp"
+#include "mips-emulator/register_file.hpp"
 
 #include <utility>
 
 namespace mips_emulator {
-    template <typename RegisterFile, typename Memory>
+    template <typename Memory>
     class Emulator {
     public:
         using Register = typename RegisterFile::Register;
-        using Address = typename RegisterFile::Unsigned;
 
         template <typename... Args>
         Emulator(Args&&... args) : memory(std::forward<Args>(args)...) {}

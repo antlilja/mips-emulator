373aee3d186f060905358b309537f13f342a7da3
Vikdos <viktor.ofjall@gmail.com>
2022-02-22 17:04:34 +0100

    Implement bitswap instruction and tests.

--- tests/executor.cpp
+++ tests/executor.cpp
@@ -542,6 +542,37 @@ TEST_CASE("jalr", "[Executor]") {
     REQUIRE(reg_file.get(RegisterName::e_ra).u == 0x10beef04);
 }
 
+TEST_CASE("bitswap", "[Executor") {
+    using R = Instruction::Special3Func;
+    using ROp = Instruction::Special3RTypeOp;
+
+    SECTION("swaps") {
+        uint32_t cases[][2] = {{0, 0},
+                               {UINT32_MAX, UINT32_MAX},
+                               {0x80808080, 0x01010101},
+                               {0xf8f8f8f8, 0x1f1f1f1f},
+                               {0x96969696, 0x69696969},
+                               {0x18181818, 0x18181818},
+                               {0xf8189680, 0x1f186901}};
+
+        for (auto& test : cases) {
+            RegisterFile reg_file;
+
+            reg_file.set_unsigned(RegisterName::e_t0, 0);
+            reg_file.set_unsigned(RegisterName::e_t1, test[0]);
+
+            const Instruction instr(R::e_bshfl, ROp::e_bitswap,
+                                    RegisterName::e_t0, RegisterName::e_t1);
+
+            const bool no_error =
+                Executor::handle_special3_rtype_instr(instr, reg_file);
+            REQUIRE(no_error);
+
+            REQUIRE(reg_file.get(RegisterName::e_t0).u == test[1]);
+        }
+    }
+}
+
 TEST_CASE("wsbh", "[Executor]") {
     using R = Instruction::Special3Func;
     using ROp = Instruction::Special3RTypeOp;

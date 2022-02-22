b04166d4f9bf153758978b6e84c07b7636a29d13
hanstig <stighannes@gmail.com>
2022-02-22 21:07:00 +0100

    Added basic tests for seb and seh instructions

--- tests/executor.cpp
+++ tests/executor.cpp
@@ -844,3 +844,72 @@ TEST_CASE("sltu", "[Executor]") {
         REQUIRE(reg_file.get(RegisterName::e_t2).s == 1);
     }
 }
+
+TEST_CASE("seh", "[Executor]") {
+    using R = Instruction::Special3Func;
+    using ROp = Instruction::Special3RTypeOp;
+
+    SECTION("Leading 1") {
+        RegisterFile reg_file;
+
+        reg_file.set_unsigned(RegisterName::e_t1, 0x0000F0F0);
+
+        const Instruction instr(R::e_bshfl, ROp::e_seh, RegisterName::e_t0,
+                                RegisterName::e_t1);
+
+        const bool no_error =
+            Executor::handle_special3_rtype_instr(instr, reg_file);
+        REQUIRE(no_error);
+
+        REQUIRE(reg_file.get(RegisterName::e_t0).u == 0xFFFFF0F0);
+    }
+
+    SECTION("Leading 0") {
+        RegisterFile reg_file;
+
+        reg_file.set_unsigned(RegisterName::e_t1, 0x7ff1);
+
+        const Instruction instr(R::e_bshfl, ROp::e_seh, RegisterName::e_t0,
+                                RegisterName::e_t1);
+
+        const bool no_error =
+            Executor::handle_special3_rtype_instr(instr, reg_file);
+        REQUIRE(no_error);
+
+        REQUIRE(reg_file.get(RegisterName::e_t0).u == 0x7ff1);
+    }
+}
+TEST_CASE("seb", "[Executor]") {
+    using R = Instruction::Special3Func;
+    using ROp = Instruction::Special3RTypeOp;
+
+    SECTION("Leading 1") {
+        RegisterFile reg_file;
+
+        reg_file.set_unsigned(RegisterName::e_t1, 0x000000F8);
+
+        const Instruction instr(R::e_bshfl, ROp::e_seb, RegisterName::e_t0,
+                                RegisterName::e_t1);
+
+        const bool no_error =
+            Executor::handle_special3_rtype_instr(instr, reg_file);
+        REQUIRE(no_error);
+
+        REQUIRE(reg_file.get(RegisterName::e_t0).u == 0xFFFFFFF8);
+    }
+
+    SECTION("Leading 0") {
+        RegisterFile reg_file;
+
+        reg_file.set_unsigned(RegisterName::e_t1, 0xFFFFFF7F);
+
+        const Instruction instr(R::e_bshfl, ROp::e_seb, RegisterName::e_t0,
+                                RegisterName::e_t1);
+
+        const bool no_error =
+            Executor::handle_special3_rtype_instr(instr, reg_file);
+        REQUIRE(no_error);
+
+        REQUIRE(reg_file.get(RegisterName::e_t0).u == 0x0000007F);
+    }
+}

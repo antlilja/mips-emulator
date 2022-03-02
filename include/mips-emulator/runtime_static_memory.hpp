454ef91e0df9edb0b282cfbf75e8edb0c06dc3c4
antlilja <liljaanton2001@gmail.com>
2022-03-02 12:50:22 +0100

    Added constructor to RuntimeStaticMemory
    Added a constructor to RuntimeStaticMemory which takes a vector.

--- include/mips-emulator/runtime_static_memory.hpp
+++ include/mips-emulator/runtime_static_memory.hpp
@@ -15,6 +15,12 @@ namespace mips_emulator {
                   std::move(mmio)),
               memory(size) {}
 
+        RuntimeStaticMemory(std::vector<uint8_t> mem,
+                            std::shared_ptr<MMIOHandler> mmio = nullptr)
+            : Memory<RuntimeStaticMemory<MMIOHandler>, MMIOHandler>(
+                  std::move(mmio)),
+              memory(std::move(mem)) {}
+
         uint8_t* get_memory() { return &memory[0]; }
         uint32_t get_size() const { return memory.size(); }
 
